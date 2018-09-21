fe2ce97bcf2343bf0597ed6aaa01694a13a1dfb5
coolkid010 <legoconsoleboy@sky.com>
2018-09-21 08:13:16 +0100

    Add files via upload

---
+++ turtlestairs.py
@@ -0,0 +1,64 @@
+#Turtle Stairs By Sean Wetton 2018 
+#Setup
+from turtle import *
+turtle = Turtle()
+turtle.shape('turtle')
+turtle.left(90)
+#Start
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+#Repeat
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+#Middle
+turtle.left(90)
+turtle.forward(90)
+#Repeat
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+#turtle.left(90) Makes it go down so its not needed
+turtle.forward(90)
+#Middle
+turtle.right(90)
+#Repeat But with left replaced with right
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+#turtle.right(90) and
+#turtle.forward(90) are not needed as it makes it go down
+turtle.left(90)
+#Repeat
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+#Middle
+turtle.left(90)
+turtle.left(90)
+#Repeat
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+#End
+turtle.left(270)
